243b064e8a9fd63dbaa0d6a51f6626becf0f3690
chinweibegbu <chinwe.ibegbu@gmail.com>
2021-11-16 15:23:53 +0100

    Implemented sample app functionality without DB connection

--- src/main/java/com/springboot/courseapp/CourseAppApplication.java
+++ src/main/java/com/springboot/courseapp/CourseAppApplication.java
@@ -2,12 +2,18 @@ package com.springboot.courseapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class CourseAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseAppApplication.class, args);
 	}
-
+	
+	@GetMapping
+	public String Demo() {
+		return "Hello, World";
+	}
 }
