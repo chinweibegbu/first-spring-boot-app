e60c145805efede80a92f4ab208e9b54f8d89652
chinweibegbu <chinwe.ibegbu@gmail.com>
2021-11-16 16:37:47 +0100

    Added basic student service

---
+++ src/main/java/com/springboot/courseapp/student/StudentService.java
@@ -0,0 +1,18 @@
+package com.springboot.courseapp.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+	
+	public List<Student> getAllStudents() {
+		return List.of(
+				new Student(1L, "Ayoola Falugba", "ayofal@gmail.com", LocalDate.of(2000, Month.FEBRUARY, 14), 20),
+				new Student(1L, "Omone Oshiafi", "oshiafi1923@loyolajesuit.org", LocalDate.of(1983, Month.JUNE, 16), 30)
+				);
+	}
+}
